45aff2b57bc7e11f7cb5139deb4f9ce36adf5357
sumantsingh <sumantsingh542@gmail.com>
2026-01-15 11:48:12 +0000

    terraform_shubham folder to github

---
+++ terraform_shubham/main.tf
@@ -0,0 +1,4 @@
+resource "local_file" "myfile" {
+  filename = "automate.txt"
+  content  = "best content for aws"
+}
---
+++ terraform_shubham/s3.tf
@@ -0,0 +1,10 @@
+#this is s3 bucket
+
+resource aws_s3_bucket my_bucket{
+      bucket = "terraform in one shot"
+
+
+
+
+}
+
